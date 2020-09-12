cb23aeac1063bea2e5667a1c5c1e90fcae6771a3
Michał Romaszkin <romaszkin13@o2.pl>
2020-09-12 16:44:39 +0200

    Add basic description

--- src/mainpage/Mainpage.tsx
+++ src/mainpage/Mainpage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Container, Col, Row, Image } from 'react-bootstrap';
+import { Container, Col, Row, Image, ProgressBar } from 'react-bootstrap';
 import { CSSTransition } from 'react-transition-group';
 const { useEffect, useState } = React;
 
@@ -11,9 +11,7 @@ import Chevron from '../assets/chevron_down.svg';
 export default function MainPage() {
     const [inProp, setInProp] = useState<boolean>(false);
     useEffect(() => {
-        window.addEventListener('scroll', () => {
-            console.log(window.scrollY);
-        });
+        window.addEventListener('scroll', () => {});
     }, []);
     return (
         <Container fluid>
@@ -53,9 +51,31 @@ export default function MainPage() {
                     </CSSTransition>
                 </Col>
             </Row>
-            <Row className="justify-content-md-center rowDescription">
-                <Col md="auto" className="description" id="description">
-                    <p>Dupa</p>
+            <Row
+                className="justify-content-md-center rowDescription"
+                id="description"
+            >
+                <Col md={4} className="rowDescription__column">
+                    <h3>O mnie</h3>
+                    <p>
+                        Mam 23 lata. Pochodzę ze Szczecina, aktualnie mieszkam w
+                        Poznaniu. W wolnym czasie uwielbiam grać w gry od
+                        Paradox Interactive oraz opiekować się roślinami. Kiedyś
+                        dużo grałem w siatkówkę, aktulanie lubię śledzić wyniki
+                        w różnych ligach.
+                    </p>
+                    <p>
+                        W technikum poznałem Angulara - od tego frameworka
+                        zaczęła się moja przygoda z techologiami webowymi.
+                        Programuję głównie w JavaScript (TypeScript). Staram się
+                        rozwijać równolegle w stronę frontu i backu.
+                    </p>
+                </Col>
+                <Col
+                    md={{ span: 4, offset: 2 }}
+                    className="rowDescription__column"
+                >
+                    <h3>Umiejętności</h3>
                 </Col>
             </Row>
         </Container>
